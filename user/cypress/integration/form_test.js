7ea75a6961dba78f9e8a2aa58e1444321ba6b796
ashurik522 <ashurik522@gmail.com>
2022-05-26 17:03:29 -0400

    through testing

--- user/cypress/integration/form_test.js
+++ user/cypress/integration/form_test.js
@@ -56,10 +56,14 @@ describe("User onboarding app", () => {
                 .should("have.value", "")
                 .type("asdf1234")
                 .should("have.value", "asdf1234")
+
+        })
+
+        it("can check terms box", () => {
             termsInput()
-                .should("not.be.checked")
-                .check()
-                .should("be.checked")
+            .should("not.be.checked")
+            .check()
+            .should("be.checked")
         })
 
         it("submit button enables when all inputs are filled out", () => {
@@ -71,7 +75,110 @@ describe("User onboarding app", () => {
             submitBtn().should("not.be.disabled")
         })
 
+        it("can submit and add new user", () => {
+            fnameInput().type("Drew")
+            lnameInput().type("Shurik")
+            emailInput().type("Drew@email.com")
+            passwordInput().type("asdf1234")
+            termsInput().check()
+            submitBtn().click()
+            cy.contains("Drew")
+            cy.contains("Shurik")
+            cy.contains("Drew@email.com")
+        })
     })
+
+    describe("checking form validation completeness", () => {
+        it("submit disabled and error display if missing first name", () => {
+            fnameInput().type("Drew")
+            lnameInput().type("Shurik")
+            emailInput().type("Drew@email.com")
+            passwordInput().type("asdf1234")
+            termsInput().check()
+            fnameInput().clear()
+            submitBtn().should("be.disabled")
+            cy.contains("Enter First Name")
+
+        })
+
+        it("submit disabled and error display if missing last name", () => {
+            fnameInput().type("Drew")
+            lnameInput().type("Shurik")
+            emailInput().type("Drew@email.com")
+            passwordInput().type("asdf1234")
+            termsInput().check()
+            lnameInput().clear()
+            submitBtn().should("be.disabled")
+            cy.contains("Enter Last Name")
+
+        })
+
+        it("submit disabled and error display if missing email", () => {
+            fnameInput().type("Drew")
+            lnameInput().type("Shurik")
+            emailInput().type("Drew@email.com")
+            passwordInput().type("asdf1234")
+            termsInput().check()
+            emailInput().clear()
+            submitBtn().should("be.disabled")
+            cy.contains("Enter Email")
+
+        })
+
+        it("submit disabled and error display if invalid email", () => {
+            fnameInput().type("Drew")
+            lnameInput().type("Shurik")
+            emailInput().type("Drew@email.com")
+            passwordInput().type("asdf1234")
+            termsInput().check()
+            emailInput().clear()
+            emailInput().type("drewemail.com")
+            submitBtn().should("be.disabled")
+            cy.contains("Valid email required")
+
+        })
+
+        it("submit disabled and error display if missing password", () => {
+            fnameInput().type("Drew")
+            lnameInput().type("Shurik")
+            emailInput().type("Drew@email.com")
+            passwordInput().type("asdf1234")
+            termsInput().check()
+            passwordInput().clear()
+            submitBtn().should("be.disabled")
+            cy.contains("Enter Password")
+
+        })
+
+        it("submit disabled and error display if password has less than 6 chars", () => {
+            fnameInput().type("Drew")
+            lnameInput().type("Shurik")
+            emailInput().type("Drew@email.com")
+            passwordInput().type("asdf1234")
+            termsInput().check()
+            passwordInput().clear()
+            passwordInput().type("asdf1")
+            submitBtn().should("be.disabled")
+            cy.contains("Password must be least 6 characters")
+
+        })
+
+        it("submit disabled and error display if terms unchecked", () => {
+            fnameInput().type("Drew")
+            lnameInput().type("Shurik")
+            emailInput().type("Drew@email.com")
+            passwordInput().type("asdf1234")
+            termsInput().check()
+            termsInput().uncheck()
+            submitBtn().should("be.disabled")
+            cy.contains("Terms required")
+
+        })
+            
+    })
+
+
+    
 
 
 
